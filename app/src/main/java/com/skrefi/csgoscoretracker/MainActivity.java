fbc326de1433c36ecb56369ba1a69cdde8f937cd
SKREFI <srazvan43@yahoo.com>
2018-02-20 12:38:00 +0200

    Finished app

--- app/src/main/java/com/skrefi/csgoscoretracker/MainActivity.java
+++ app/src/main/java/com/skrefi/csgoscoretracker/MainActivity.java
@@ -1,11 +1,18 @@
 package com.skrefi.csgoscoretracker;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean someoneWon = false;
+    int duration = Toast.LENGTH_SHORT;
+    int ctScore = 0;
+    int tScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,27 +20,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int ctScore=0;
-    int tScore=0;
-
     public void addPointCT(View view) {
-        if(ctScore>=0)
-            ctScore++;
-        displayCTPoints(ctScore);
+        if (someoneWon == false) {
+            Context context = getApplicationContext();
+            Toast toastCT = Toast.makeText(context, "CT Won", duration);
+            if (ctScore >= 0 && ctScore < 16)
+                ctScore++;
+            if (ctScore == 16) {
+                toastCT.show();
+                someoneWon = true;
+            }
+            displayCTPoints(ctScore);
+        }
     }
 
     public void addPointT(View view) {
-        if(tScore>=0)
-            tScore++;
-        displayTPoints(tScore);
+        if (someoneWon == false) {
+            Context context = getApplicationContext();
+            Toast toastT = Toast.makeText(context, "T Won", duration);
+            if (tScore >= 0 && tScore < 16)
+                tScore++;
+            if (tScore == 16) {
+                toastT.show();
+                someoneWon = true;
+            }
+            displayTPoints(tScore);
+        }
     }
 
-    private void displayCTPoints(int number) {
+    public void resetScore(View view) {
+        tScore = 0;
+        ctScore = 0;
+        displayTPoints(tScore);
+        displayCTPoints(ctScore);
+        someoneWon = false;
+    }
+
+    private void displayTPoints(int number) {
         TextView ctPointsTextView = findViewById(R.id.ctScoreTextView);
         ctPointsTextView.setText("" + number);
     }
 
-    private void displayTPoints(int number) {
+    private void displayCTPoints(int number) {
         TextView tPointsTextView = findViewById(R.id.tScoreTextView);
         tPointsTextView.setText("" + number);
     }
